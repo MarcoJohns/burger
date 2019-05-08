9192477c68777b5766631f939bb857df20eb6f5f
marco <marcobjohns@gmail.com>
2019-05-08 10:23:56 -0600

    I Created my bergur repository on github.  Then i created a package.json and npm installed the packages i believe i will need to complete the assingment.  I also created a server.js file and am setting up the basic thing i will need in it. requiring and setting variable to the packages i have installed.

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var exphbs = require('express-handlebars');
+var mysql = require("mysql");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
