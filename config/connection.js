47d4272eb73d22777a99c4ae7d3bafb60b5ed7f4
marco <marcobjohns@gmail.com>
2019-05-13 21:42:59 -0600

    changed somthing

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "1qaz!QAZ",
+    password: "heroku",
     database: "burgers_db"
 });
 
